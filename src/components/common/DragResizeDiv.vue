c695f1aa5862025aa568a234553e44d3e4aade5a
冰风 <wuxiaoming@baosight.com>
2020-06-22 15:20:09 +0800

    改为由数据库进行配置

--- src/components/common/DragResizeDiv.vue
+++ src/components/common/DragResizeDiv.vue
@@ -1,10 +1,10 @@
 
 <template>
   <vue-draggable-resizable
-    :w="width"
-    :h="height"
-    :x="x"
-    :y="y"
+    :w="m_item.attr.width"
+    :h="m_item.attr.height"
+    :x="m_item.attr.x"
+    :y="m_item.attr.y"
     @dragstop="onDrag"
     @resizestop="onResize"
     :scale="scale"
@@ -24,25 +24,18 @@ import { mapState } from "vuex";
 import { reqPutCustomComponent } from "@/api";
 export default {
   props: ["item"],
-  data() {
-    return {
-      width: 600,
-      height: 600,
-      x: 0,
-      y: 0
-    };
-  },
   computed: {
-    ...mapState(["scale"])
-  },
-  created() {
-    this.mergeAttr();
+    ...mapState(["scale"]),
+    m_item() {
+      let _item = { ...this.item };
+      return _item;
+    }
   },
   methods: {
     // 调整大小
     onResize(x, y, width, height) {
       // 这里的参数，都将存入数据库，用于update
-      let _id = this.item._id;
+      let _id = this.m_item._id;
       let attr = {
         x,
         y,
@@ -69,12 +62,12 @@ export default {
     // 调整方位
     onDrag(x, y) {
       // 这里的参数，都将存入数据库，用于update
-      let _id = this.item._id;
+      let _id = this.m_item._id;
       let attr = {
         x,
         y,
-        width: this.width,
-        height: this.height
+        width: this.m_item.attr.width,
+        height: this.m_item.attr.height
       };
       reqPutCustomComponent({
         _id,
@@ -95,18 +88,7 @@ export default {
     },
     // 当激活时
     onActivated() {
-      // this.$store.commit("setOption", this.option);
-    },
-    // 合并传入的参数
-    mergeAttr() {
-      // 合并属性
-      if (this.item.attr) {
-        let { x, y, width, height } = this.item.attr;
-        x && (this.x = x);
-        y && (this.y = y);
-        width && (this.width = width);
-        height && (this.height = height);
-      }
+      // this.$store.commit("setOption", this.item);
     }
   }
 };
